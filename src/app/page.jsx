126b522a10fa22e281e1ffd60dff699d8361b6da
24HP7578 24HP7578 <24HP7578@admins-iMac.local>
2024-11-27 16:48:33 +0800

    add mongoose schema

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+  return (
+   <div>
+    <div className="w-[935px] h-[939px]">
+      <div className="w-[350px]">`` 
+
+      </div>
+    </div>
+   </div>
+  );
+}
